ef42a5bee5ce1387e162dcb4c7cde0d646e2cb8a
Oksana <oxana2000465@gmail.com>
2023-03-23 16:08:52 +0200

    Created parent and child components
    had a look how we can pass data from parent to child component

---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,32 @@
+<template>
+<h1>Parent Component</h1>
+<ChildComponent name="test_name" :data="user" :getData="getData"/>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue';
+export default {
+    name: "ParentComponent",
+    components: { ChildComponent },
+    data() {
+        return {
+            name: 'lorem',
+            user: {
+                name: 'test_name',
+                email: 'test_email'
+            }
+        }
+    },
+    methods: {
+                getData() {
+                    console.log('function called')
+                }
+            }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: orangered;
+}
+</style>
